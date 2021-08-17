aaaec80600e5323e4454bcbcd15581baeb273ab4
Lee JaeHyun <myleje@gmail.com>
2021-08-17 15:37:43 +0900

    .

--- Assets/02.Scripts/MummyAgent.cs
+++ Assets/02.Scripts/MummyAgent.cs
@@ -21,7 +21,7 @@ public class MummyAgent : Agent
         rb = GetComponent<Rigidbody>();
         stageManager = tr.parent.GetComponent<StageManager>();
 
-        MaxStep = 50;
+        MaxStep = 2000;
     }
 
     public override void OnEpisodeBegin()
@@ -40,10 +40,54 @@ public class MummyAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
+        var action = actions.DiscreteActions;
+
+        Vector3 dir = Vector3.zero;
+        Vector3 rot = Vector3.zero;
+
+        // Branch 0
+        switch (action[0])
+        {
+            case 1: dir = tr.forward; break;
+            case 2: dir = -tr.forward; break;
+        }
+        // Branch 1
+        switch (action[1])
+        {
+            case 1: rot = -tr.up; break; //왼쪽 회전
+            case 2: rot = tr.up; break;  //오른쪽 회전
+        }
+
+        tr.Rotate(rot, Time.fixedDeltaTime * turnSpeed);
+        rb.AddForce(dir * moveSpeed, ForceMode.VelocityChange);
+
+        // 마이너스 패널티
+        AddReward(-1 / (float)MaxStep); // 2000 -> 0.002 
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        var actions = actionsOut.DiscreteActions;
+        actions.Clear();
+
+        // Branch 0 - 이동 (정지/전진/후진) 0, 1, 2 : Size 3
+        if (Input.GetKey(KeyCode.W))
+        {
+            actions[0] = 1; //전진
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            actions[0] = 2; //후진
+        }
+        // Branch 1 - 회전 (정지/왼쪽회전/오른쪽회전) 0, 1, 2 : Size 3
+        if (Input.GetKey(KeyCode.A))
+        {
+            actions[1] = 1; //왼쪽 회전
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            actions[1] = 2; //오른쪽 회전
+        }
     }
 
 }
